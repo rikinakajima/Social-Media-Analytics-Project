992d053f037040d06111e5815e7f3d78af24e4ee
unknown <riki.nakajima@gmail.com>
2022-07-31 15:27:05 -0400

    A1: Social Media Analytics Project

---
+++ task_twitter/config_twitter.py
@@ -0,0 +1,9 @@
+## Twitter API credentials.
+
+# Consumer keys (API key and secret)
+consumer_key = 'P4Drp0A5HHKUC8VNav4PkqL7r'
+consumer_secret = 'jXF8OCYpGqAqDeCNLKascykOFT2QWLkyXr3df0H9Ptm3fhwaKT'
+
+# Application access token key and access token secret
+access_token = '1551675144241807360-I6ykONIaLFqLExhZfCtwPNdU6g4nEP'
+access_token_secret = 'W8qksy9CpofIMfG2k86ay54xBsHcmOSuYZ9tiujfa4WLe'
